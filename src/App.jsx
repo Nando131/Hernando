8722d13760331911e0fd688e4438f25e769cb5d8
Nando131 <Nandoher00@gmail.com>
2024-11-13 13:30:21 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ import './App.css'
 
 import React from 'react';
 
-function Greeting({ name }) {
-  return <div>Hello, {name}!</div>;
+function MyComponent({ stringProp, numberProp, booleanProp, arrayProp, objectProp }) {
+  return (
+    <div>
+      <p>String Prop: {stringProp}</p>
+      <p>Number Prop: {numberProp}</p>
+      <p>Boolean Prop: {booleanProp ? 'True' : 'False'}</p>
+      <p>Array Prop:</p>
+      <ul>
+        {arrayProp.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <p>Object Prop:</p>
+      <p>Name: {objectProp.name}</p>
+      <p>Age: {objectProp.age}</p>
+    </div>
+  );
 }
-
-<Greeting name="Alice" />
 
 

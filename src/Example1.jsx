4d6f567398d81a5dac61b1cc544c97d73d0c4939
Nando131 <Nandoher00@gmail.com>
2024-11-13 13:39:56 +0700

    Initial commit

---
+++ src/Example1.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Greeting({ name, age, isLoggedIn, hobbies, address }) {
+  return (
+    <div>
+      <p>Hello, {name}!</p>
+      <p>You are {age} years old.</p>
+      {isLoggedIn ? <p>You are logged in.</p> : null}
+      <p>Hobbies:</p>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
+      <p>Address:</p>
+      <p>Street: {address.street}</p>
+      <p>City: {address.city}</p>
+    </div>
+  );
+}

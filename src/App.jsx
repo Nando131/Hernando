a2c27573a4dd79792dffe4a08eaee9cf9d39317a
Nando131 <Nandoher00@gmail.com>
2024-11-13 13:24:36 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,4 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import React from 'react';
+
+function Greeting({ name }) {
+  return <div>Hello, {name}!</div>;
+}
+
+<Greeting name="Alice" />
+
 
